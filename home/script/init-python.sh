495e8374b23fa950a7ca7fa2857c050b217240e6
Chase Kernan <chase.kernan@gmail.com>
2014-12-28 14:16:00 -0700

    script/init-python.sh

---
+++ home/script/init-python.sh
@@ -0,0 +1,12 @@
+
+pyenv install 3.4.2
+pyenv install 2.7.9
+
+pyenv global 3.4.2
+
+pip install numpy
+pip install scipy
+pip install ipython
+pip install cython
+pip install numexpr
+pip install pandas
